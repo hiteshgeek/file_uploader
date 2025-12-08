71bae31af69407d4a76f9ca188cf1e845a09cd11
Hitesh Vaghela <hbvaghela83@gmail.com>
2025-12-08 22:32:59 +0530

    fixed issue in with config builder dev

--- projects/file-uploader/config-builder.php
+++ projects/file-uploader/config-builder.php
@@ -7,6 +7,9 @@ include_once __DIR__ . '/../../includes/functions.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title>FileUploader Configuration Builder</title>
     <link rel="icon" type="image/svg+xml" href="../../src/assets/images/download.svg">
 
